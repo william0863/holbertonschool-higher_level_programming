9eaf193ad079d740f0f5cfdcc268cad4f5279304
william0863 <granger.w35@gmail.com>
2022-09-25 21:52:46 +0200

    task 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+"""
+Matrix Manipulation
+"""
+
+def matrix_divided(matrix, div):
+    """Divide all elements of the matrix
+    Parameters:
+    matrix = matrix
+    div = integer dividing elements
+    Local Args:
+    new = list of lists
+    array = lists with new elements
+    """
+
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+    if not div:
+        raise ZeroDivisionError('division by zero')
+
+    new = []
+    array = []
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
+        if not all(row) and all(matrix[row]) is False:
+            raise TypeError(
+                'matrix must be a matrix (list of lists) of integers/floats')
+        for num in row:
+            if isinstance(num, (int, float)) is False:
+                raise TypeError(
+                    'matrix must be a matrix (list of lists) of integers/floats')
+            array.append(round(num / div, 2))
+        new.append(array)
+        array = []
+    return new
