f2dec9d6e3377e3482ecca07420b2827855766f4
william0863 <granger.w35@gmail.com>
2022-10-03 14:31:46 +0200

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ script that adds all arguments to a Python list
 and then save them to a file
 """
 import sys
+import json
 
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
