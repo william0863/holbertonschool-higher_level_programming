88f2786f92b20800a47dbafb473be58508e52329
william0863 <granger.w35@gmail.com>
2022-10-03 14:11:33 +0200

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that returns an object (Python data structure)
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure)
+represented by a JSON string
+    args:
+        my_str: string to convert
+    return:
+        object
+    """
+
+    return json.loads(my_str)
