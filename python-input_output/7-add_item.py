aa23a6d7172755e87c27e2fb517e85bc1fc68054
william0863 <granger.w35@gmail.com>
2022-10-03 15:22:04 +0200

    task 7 fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""
- script that adds all arguments to a Python list, and then save them to a file
+"""Module 7-add_item.py
+ script that adds all arguments to a Python list
+ and then save them to a file
 """
 import sys
 import json
