2d61dc1685dd05d47164548f68f5758122431224
william0863 <granger.w35@gmail.com>
2022-10-03 13:01:05 +0200

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Module 1-number_of_lines
+Module 1-write_file
 function that writes a string to a text file and
 returns the number of char written
 """
 
 
-def number_of_lines(filename=""):
-    """Return number of lines in text file"""
+def write_file(filename="", text=""):
+    """function that writes a string to a text file"""
     with open(filename, mode="r", encoding="utf-8") as f:
         lines = 0
         for line in f:
