c64c2e0f380b1eb57ca528877abc05a5fc20f57d
william0863 <granger.w35@gmail.com>
2022-10-03 13:03:14 +0200

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,8 +8,5 @@ returns the number of char written
 
 def write_file(filename="", text=""):
     """function that writes a string to a text file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        lines = 0
-        for line in f:
-            lines += 1
-    return lines
+    with open(filename, mode="w", encoding="utf-8") as f:
+    return f.write(text)
