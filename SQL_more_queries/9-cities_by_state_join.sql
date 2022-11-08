addc20cd69dc6be4768f50455fd836c594d8d666
william0863 <granger.w35@gmail.com>
2022-11-08 15:48:28 +0100

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -6,7 +6,7 @@
 
 
 	SELECT cities.id, cities.name, states.name
-	    FROM cities
-	    JOIN states
-	  WHERE cities.state_id = states_id
+	    FROM states
+	    JOIN cities
+	  ON states.id = cities.state_id
 ORDER BY cities.id ASC;
