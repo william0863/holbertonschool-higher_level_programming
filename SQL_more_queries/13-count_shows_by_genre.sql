d8e97f4218ae0eb6a0b80486a3968fd029173ff4
william0863 <granger.w35@gmail.com>
2022-11-09 00:24:57 +0100

    task13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,7 +9,7 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
+SELECT tv_genres.name AS genre, COUNT(*) AS 'number_of_shows'
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
