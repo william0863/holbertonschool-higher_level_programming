fe9558695e3a53ec4861af79dc9b45a263f44d5d
william0863 <granger.w35@gmail.com>
2022-12-09 21:28:31 +0100

    task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (!size) {
+  console.log('Missing size');
+}
+let str = '';
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    str += 'X';
+  }
+  console.log(str);
+  str = '';
+}
