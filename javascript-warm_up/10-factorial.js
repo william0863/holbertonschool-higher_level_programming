6399065b3ec1eaf35a3015c3c92ccf4f202cd809
william0863 <granger.w35@gmail.com>
2022-12-09 22:22:21 +0100

    task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let n = parseInt(process.argv[2]);
+function factorial (n) {
+  if (n === 1 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+let final = factorial(n);
+console.log(final);
