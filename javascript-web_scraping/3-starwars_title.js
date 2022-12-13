db2af212599d8a4453a932a4d97250e3b6db9d90
william0863 <granger.w35@gmail.com>
2022-12-13 12:33:01 +0100

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const r = require('request');
-const u = 'http://swapi.co/api/films/' + process.argv[2];
+const u = 'https://swapi-api.hbtn.io/api/films/:id' + process.argv[2];
 r(u, function (error, response, body) {
   console.log(JSON.parse(body).title || error);
 });
